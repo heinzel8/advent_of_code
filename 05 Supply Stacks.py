89a6f60fe03c80f7f3b13f661f9bfbdca8699ca3
Christian Grambs <christian.grambs@leica-geosystems.com>
2022-12-05 12:58:24 +0100

    finished day 5

--- 05 Supply Stacks.py
+++ 05 Supply Stacks.py
@@ -1,21 +1,19 @@
-from pathlib import Path
 import re
+import copy
 
 def read_stacks():
     with open("05 Stacks.txt", encoding="utf8") as f:
         lines = [line.strip() for line in f.readlines()]
     stacks = []
-    for i in range(9):
+    for index in range(9):
         stacks.append([])
     for line in lines:
         for n in range(9):
-            i = n*4+1
-            if i > len(line):
+            index = n*4+1
+            if index > len(line) or (line[index]) == " ":
                 continue
-            if (line[i]) == " ":
-                continue
-            stacks[n].append(line[i])
-    return stacks
+            stacks[n].append(line[index])
+    return [list(reversed(s)) for s in stacks]
 
 def read_moves():
     with open("05 Moves.txt", encoding="utf8") as f:
@@ -23,6 +21,7 @@ def read_moves():
     moves = []
     for line in lines:
         moves.append([int(n) for n in re.findall(r"\d+", line)])
+    return moves
 
 def get_input_data():
     s = read_stacks()
@@ -30,21 +29,40 @@ def get_input_data():
     return s, m
 
 def get_test_data():
-    return ["", ""]
+    stacks = [["N", "Z"], ["D", "C", "M"], ["P"]]
+    stacks = [list(reversed(s)) for s in stacks]
+    moves = [[1, 2, 1],
+             [3, 1, 3],
+             [2, 2, 1],
+             [1, 1, 2]]
+    return stacks, moves
 
 def solve_puzzle(stacks, moves):
-    print(moves)
-    return "", ""
+    stacks1 = copy.deepcopy(stacks)
+    for move in moves:
+        c, f, t = move
+        for i in range(c):
+            stacks1[t-1].append(stacks1[f-1].pop())
+    s1 = "".join([s[-1] for s in stacks1])
+
+    stacks2 = copy.deepcopy(stacks)
+    for move in moves:
+        c, f, t = move
+        to_move = stacks2[f-1][-c:]
+        stacks2[t-1].extend(to_move)
+        del(stacks2[f-1][-c:])
+    s2 = "".join([s[-1] for s in stacks2])
+    return s1, s2
 
 def test_reference():
-    res1, res2 = solve_puzzle(get_test_data())
-    #assert res1 == 0
-    #assert res1 == 0
+    res1, res2 = solve_puzzle(*get_test_data())
+    assert res1 == "CMZ"
+    assert res2 == "MCD"
 
 def test_solution():
-    res1, res2 = solve_puzzle(get_test_data())
-    #assert res1 == 0
-    #assert res2 == 0
+    res1, res2 = solve_puzzle(*get_input_data())
+    assert res1 == "TDCHVHJTG"
+    assert res2 == "NGCMPJLHV"
 
 if (__name__ == "__main__"):
     s1, s2 = solve_puzzle(*get_input_data())
