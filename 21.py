35d7aa08ca9c2267faf7270594f7b39fb2b3eea5
chris <chris@grambs.info>
2022-12-28 13:46:31 +0100

    finished day 21

--- 21.py
+++ 21.py
@@ -5,24 +5,21 @@ from copy import deepcopy
 
 class Monkey:
     def __init__(self, name:str, value:str) -> None:
-        if name == "dcjm":
-            pass
-        name = "monkey_" + name
         self.name = name
         if value.isnumeric():
             self.number = int(value)
         else:
-            pattern_instruction = "([a-z]+) ([*\-+/]+) (.*)"
-            match = re.match(pattern_instruction, value).groups()
-            assert len(match) == 3
-            self.source1, self.operation, self.source2 = match
-            self.expression = value.replace(self.source1, "monkey_" + self.source1).replace(self.source2, "monkey_" + self.source2)
-            self.source1 = "monkey_" + self.source1
-            self.source2 = "monkey_" + self.source2
+            self.set_expression(value)
+
+    def set_expression(self, value):
+        self.sources = [s.strip() for s in re.split(r'[*\-\+/]', value) if not s.strip().isnumeric()]
+        match = re.match(".*([*\-\+/]+).*", value)
+        self.operation = match.groups()[0]
+        self.expression = value
+
     name = ""
     number = None
-    source1 = ""
-    source2 = ""
+    sources:list[str] = []
     operation = None
     expression = ""
 
@@ -30,8 +27,14 @@ def preprocess(puzzle):
     pattern = "(.*): (.*)"
     return [Monkey(*re.match(pattern, line).groups()) for line in puzzle]
 
-def solve_puzzle(_monkeys:list[Monkey]):
-    monkeys = deepcopy(_monkeys)
+def get_monkey(name, monkeys):
+    r = [m for m in monkeys if m.name == name]
+    if not r:
+        raise ValueError(f"monkey with name {name} not found.")
+    return r[0]
+
+    
+def solve_puzzle_part1(monkeys:list[Monkey]):
     has_unresolved = True
     while has_unresolved:
         has_unresolved = False
@@ -39,63 +42,67 @@ def solve_puzzle(_monkeys:list[Monkey]):
             if monkey.number is not None:
                 continue
             else:
-                sm1:Monkey = [m for m in monkeys if m.name == monkey.source1][0]
-                sm2:Monkey = [m for m in monkeys if m.name == monkey.source2][0]
+                sm1:Monkey = [m for m in monkeys if m.name == monkey.sources[0]][0]
+                sm2:Monkey = [m for m in monkeys if m.name == monkey.sources[1]][0]
                 if sm1.number is not None and sm2.number is not None:
-                    monkey.number = int(eval(f"{sm1.number} {monkey.operation} {sm2.number}"))
+                    s = f"{sm1.number} {monkey.operation} {sm2.number}"
+                    monkey.number = int(eval(s))
                 else:
                     has_unresolved = True
-    s1 = [m for m in monkeys if m.name == "monkey_root"][0].number
+    s1 = [m for m in monkeys if m.name == "root"][0].number
+    return s1
 
-    # symbols = []
-    # equations = []
-    # monkeys = deepcopy(_monkeys)
-    # human:Monkey = [m for m in monkeys if m.name == "monkey_humn"][0]
-    # human.number = None
-    # human.expression = "x"
-    # x = S("x")
-    # for i,m in enumerate(monkeys):
-    #     if m.name == "monkey_root":
-    #         l = m.source1
-    #         r = m.source2
-    #     elif m.number is not None:
-    #         l = m.name
-    #         r = m.number
-    #     else:
-    #         l = m.name
-    #         r = m.expression
-    #     equations.append(Eq(S(l), S(r)))
-    #     print(f"{i:4} {l} = {r}")
-    #     if 0 == i % 100:
-    #         pass
-    #         # result = solve(equations)
-    # result = solve(equations)
-    # s2 = result[0]
-    # print(s2["x"])
-    # print(type(s2))
-    s2 = None
+def solve_puzzle_part2(monkeys:list[Monkey]):
+    symbols = []
+    equations = []
+    human:Monkey = get_monkey("humn", monkeys)
+    human.number = None
 
+    resolved = False
+    while True:
+        resolved = False
+        for monkey in [m for m in monkeys if m.number is None and m.name != "humn"]:
+            for source_name in monkey.sources:
+                source_monkey:Monkey = get_monkey(source_name, monkeys)
+                if source_name not in monkey.expression:
+                    continue
+                if source_monkey.number is not None:
+                    monkey.set_expression(monkey.expression.replace(source_name, str(source_monkey.number)))
+                    resolved = True
+            try:
+                s = eval(monkey.expression)
+                monkey.number = int(s)
+                resolved = True
+            except:
+                pass
+        
+        if not resolved: break
+    
+    for i,monkey in enumerate([m for m in monkeys if m.name != "humn"]):
+        if monkey.name == "root":
+            l,r = monkey.expression.split(f" {monkey.operation} ")
+        elif monkey.number is not None:
+            continue
+            l = monkey.name
+            r = monkey.number
+        else:
+            l = monkey.name
+            r = monkey.expression
+        equations.append(Eq(S(l), S(r)))
+        #print(f"{i:4} {l} = {r}")
+
+    result = solve(equations)
+    return int(result.get(S("humn")))
+
+def solve_puzzle(_monkeys:list[Monkey]):
+    s1, s2 = None, None
+    s1 = solve_puzzle_part1(deepcopy(_monkeys))
+    s2 = solve_puzzle_part2(deepcopy(_monkeys))
     return s1, s2
 
 if (__name__ == "__main__"):
-    x, y = S(["x", "y"])
-    #equations = [Eq(x,y), Eq(y, 5**2)]
-    equations = []
-    # x = S("x")
-    # a = S("pppw")
-    # b = S("sjmn")
-    # s = S("root")
-    equations.append(Eq(S("myroot"), S("pppw + sjmn")))
-    equations.append(Eq(S("pppw"), 1))
-    equations.append(Eq(S("sjmn"), 1))
-    
-    #dbpl: 5
-    #cczh: sllz + lgvd
-
-    print(solve(equations))
-
     ref = solve_puzzle(preprocess(get_reference_data(__file__)))
-    print_statistics("Reference", ref, expected=(152, None))
+    print_statistics("Reference", ref, expected=(152, 301))
     
-    #sol = solve_puzzle(preprocess(get_puzzle_data(__file__)))
-    #print_statistics("Solution",  sol, expected=(81075092088442, None))
+    sol = solve_puzzle(preprocess(get_puzzle_data(__file__)))
+    print_statistics("Solution",  sol, expected=(81075092088442, 3349136384441))
