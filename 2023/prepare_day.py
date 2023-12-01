3d1020367d82035690dabe0aac6685a939c0911d
chris <chris@grambs.info>
2023-12-01 07:51:38 +0100

    preparation for 2023

---
+++ 2023/prepare_day.py
@@ -0,0 +1,33 @@
+"""script to prepare files for advent of code"""
+import sys, os
+import shutil as sh
+from aocd import get_data, examples
+from pathlib import Path
+
+if len(sys.argv) != 2:
+    raise ValueError('Please provide day number')
+
+YEAR = 2023
+DAY = sys.argv[1]
+SCRIPT_TEMPLATE_FILE = "template.py"
+SCRIPT_FILE = f"{DAY}.py"
+DATA_FILE = f"{DAY}.txt"
+REFERENCE_DATA_FILE = f"{DAY}_reference.txt"
+
+if not os.path.exists(SCRIPT_TEMPLATE_FILE):
+    raise FileNotFoundError(f"{SCRIPT_TEMPLATE_FILE} not found")
+
+os.makedirs(BASE_PATH, exist_ok=True)
+
+if not os.path.exists(SCRIPT_FILE):
+    sh.copy(SCRIPT_TEMPLATE_FILE, SCRIPT_FILE)
+    print(f"created {SCRIPT_FILE}")
+else:
+    print(f"{SCRIPT_FILE} already exists")
+
+with open(DATA_FILE, 'w', encoding="utf8") as f:
+    f.write(get_data(day=int(DAY), year=YEAR))
+print(f"created {DATA_FILE}")
+with open(REFERENCE_DATA_FILE, 'w', encoding="utf8") as f:
+    pass
+print(f"created {REFERENCE_DATA_FILE}")
