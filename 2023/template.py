7c6553c231f2320dc86582880fe51bac6e533f0d
chris <chris@grambs.info>
2023-12-03 19:41:29 +0100

    improved template

--- 2023/template.py
+++ 2023/template.py
@@ -3,8 +3,9 @@ from aoc_utils import get_puzzle_data, get_reference_data, print_statistics
 def preprocess(puzzle):
     return puzzle
 
-def solve_puzzle(puzzle, part):
-    return None, None
+def solve_puzzle(puzzle):
+    part1 = part2 = None
+    return part1, part2
 
 def run_tests():
     pass
@@ -12,10 +13,10 @@ def run_tests():
 if (__name__ == "__main__"):
     run_tests()
 
-    ref1 = solve_puzzle(get_reference_data(__file__, part=1), part=1)[0]
-    ref2 = solve_puzzle(get_reference_data(__file__, part=2), part=2)[1]
+    ref1 = ref2 = None
+    ref1 = solve_puzzle(get_reference_data(__file__, part=1))[0]
+    # ref2 = solve_puzzle(get_reference_data(__file__, part=2))[1]
     print_statistics("Reference", (ref1, ref2), expected=(None, None))
 
-    sol1 = solve_puzzle(get_puzzle_data(__file__), part=1)
-    sol2 = solve_puzzle(get_puzzle_data(__file__), part=2)
-    print_statistics("Solution", (sol1, sol2), expected=(None, None))
+    # sol = solve_puzzle(get_puzzle_data(__file__))
+    # print_statistics("Solution", sol, expected=(None, None))
