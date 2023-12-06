5467f97868e973089d54068a121e71a3dd6b9124
chris <chris@grambs.info>
2023-12-06 18:48:09 +0100

    day 5 finished

--- 2023/5.py
+++ 2023/5.py
@@ -8,10 +8,6 @@ class RangeData:
     dest_range_start: int
     source_range_start: int
     range_length: int
-    def get_source_range(self):
-        return range(self.source_range_start, self.source_range_start + self.range_length + 1)
-    def get_dest_range(self):
-        return range(self.dest_range_start, self.dest_range_start + self.range_length + 1)
     def in_source_range(self, value):
         return value >= self.source_range_start and value < self.source_range_start + self.range_length
     def in_target_range(self, value):
@@ -21,29 +17,30 @@ class RangeData:
 class MapData:
     name : str
     ranges: list = field(default_factory=list)
-    def transform(self, source_value):
+    def transform(self, value):
         for r in self.ranges:
-            # sr = r.get_source_range()
-            # dr = r.get_dest_range()
-            if r.in_source_range(source_value):
-                diff = source_value - r.source_range_start
+            if r.in_source_range(value):
+                diff = value - r.source_range_start
                 return r.dest_range_start + diff
+        return value
 
-                # index = sr.index(source_value)
-                # res = dr[index]
-                return res
-        return source_value
+    def calculate_jump(self, value):
+        for r in self.ranges:
+            if r.in_source_range(value):
+                return r.source_range_start + r.range_length - value
+        return 1e100
+
 
 def preprocess(puzzle):
     maps = []
     current_map = None
     for line in puzzle:
-        if "seeds:" in line:
-            seeds_1 = list(map(int, line.replace("seeds:", "").split()))
+        if (pattern:="seeds:") in line:
+            seeds_1 = list(map(int, line.replace(pattern, "").split()))
             ranges = list(map(lambda x: x.split(), [r for r in re.findall("\d+ \d+", line)]))
             seeds_2 = [RangeData(0, int(r[0]), int(r[1])) for r in ranges]
-        elif "map:" in line:
-            current_map = MapData(line.replace(" map:", ""), [])
+        elif (pattern:=" map:") in line:
+            current_map = MapData(line.replace(pattern, ""), [])
         elif len(line) == 0:
             if current_map is not None:
                 maps.append(current_map)
@@ -65,20 +62,18 @@ def solve_puzzle(puzzle):
 
     part1 = min_location
 
-    c = 0
-    for seed_range in seeds_2:
-        c += seed_range.range_length
-
-    c1 = 0
     min_location = sys.maxsize
     for seed_range in seeds_2:
-        for seed in seed_range.get_source_range():
-            c1 += 1
-            if c1 % 100_000 == 0:
-                print(f"\r{c1/c*100:0.3} {c1}/{c}         ", end="")
+        seed = seed_range.source_range_start
+        while seed < seed_range.source_range_start + seed_range.range_length:
+            jump = sys.maxsize
+            s = seed
             for m in maps:
-                seed = m.transform(seed)
-            min_location = min(min_location, seed)
+                jump = min(m.calculate_jump(s), jump)
+                s = m.transform(s)
+            min_location = min(min_location, s)
+            # skip a range of numbers that are converted to consecutive numbers
+            seed += 1 if jump == sys.maxsize or jump < 1 else jump
 
     part2 = min_location
 
@@ -92,9 +87,9 @@ if (__name__ == "__main__"):
     run_tests()
 
     ref1 = ref2 = None
-    # ref1 = solve_puzzle(get_reference_data(__file__, part=1))[0]
-    # ref2 = solve_puzzle(get_reference_data(__file__, part=2))[1]
-    # print_statistics("Reference", (ref1, ref2), expected=(35, 46))
+    ref1 = solve_puzzle(get_reference_data(__file__, part=1))[0]
+    ref2 = solve_puzzle(get_reference_data(__file__, part=2))[1]
+    print_statistics("Reference", (ref1, ref2), expected=(35, 46))
 
     sol = solve_puzzle(get_puzzle_data(__file__))
-    print_statistics("Solution", sol, expected=(510109797, None))
+    print_statistics("Solution", sol, expected=(510109797, 9622622))
